bcc63248b37db4cac3de537ac4b88c4d706234c3
ethanweed <weed.ethan@gmail.com>
2020-04-04 13:20:07 +0200

    initial commit

---
+++ PsychoPy3 Demos/stroop/scripts/assemble_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import glob
+import re
+
+path = '/Users/ethan/Documents/GitHub/Learn-PsychoPy/PsychoPy3 Demos/stroop/data'
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    #match = re.search('(?<=/)\d+', filename)
+    #idnum = match.group()
+    #df['ID'] = idnum
+    df = df[df['resp.rt'].notna()]
+    li.append(df)
+
+df = pd.concat(li, axis=0, ignore_index=True)
+
+out = path + '/all_data.csv'
+df.to_csv(out, sep=',', encoding='utf-8')
+
+
+data = df['resp.rt'].groupby([df['participant'], df['congruent']]).mean()
+
+data = data.to_frame()
+
+out = path + '/all_data_agg.csv'
+data.to_csv(out, sep=',', encoding='utf-8')
